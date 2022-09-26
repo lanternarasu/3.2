6997b22f56724d59b8670ccbea7e13c61e19e349
Admin <Admin@DESKTOP-RTHB91U>
2022-09-26 13:19:04 +0530

    Add project files.

---
+++ 3.2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _3._2
+{
+    public class Class1
+    {
+        public static void Main(String[] args)
+        {
+            Class2 obj1 = new Class2();
+            obj1.get_volume(2);
+            obj1.get_volume(2, 3);
+            obj1.get_volume(2, 3, 4);
+        }
+    }
+}
---
+++ 3.2/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2
+{
+    internal class Class2
+    {
+        
+        public void get_volume(int a)
+        {
+            int cube = a * a * a;
+            Console.WriteLine(cube);
+        }
+        public void get_volume(int l,int w,int h)
+        {
+            int cuboid = l * w * h;
+            Console.WriteLine(cuboid);
+        }
+        public void get_volume(int r, int h)
+        {
+            double cylinder = 3.14 * r * r * h;
+            Console.WriteLine(cylinder);
+        }
+    }
+}
